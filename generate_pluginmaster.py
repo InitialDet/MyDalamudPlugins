dd9773cf01f001d5e6a0661f42f13c09a297277d
Det <13919114+InitialDet@users.noreply.github.com>
2022-04-22 02:55:58 -0300

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -6,7 +6,7 @@ from os.path import getmtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
 BRANCH = os.environ['GITHUB_REF'].split('refs/heads/')[-1]
-DOWNLOAD_URL = 'https://raw.githubusercontent.com/InitialDet/DalamudPlugins/{branch}/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://raw.githubusercontent.com/InitialDet/MyDalamudPlugins/{branch}/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
